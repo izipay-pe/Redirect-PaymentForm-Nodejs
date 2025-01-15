66f1f0390e236676fd78c81b75ad2c868ffbbf48
izipay-pe <103148193+izipay-pe@users.noreply.github.com>
2025-01-15 03:00:42 -0600

    Keys commit

---
+++ keys/keys.js
@@ -0,0 +1,9 @@
+const keys = {
+    // Identificador de la tienda
+    "SHOP_ID" : "~ CHANGE_ME_USER_ID ~",
+
+    // Clave de Test o Producción
+    "KEY" : "~ CHANGE_ME_KEY ~"
+}
+
+module.exports = keys
